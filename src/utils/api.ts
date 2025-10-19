35c411fd2e01d21a25e55b427f14ef52f9d65ae0
akankshagupta221b <akanshagupta09876@gmail.com>
2025-10-19 15:11:59 +0530

    fix: Added backend url to api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "https://sareeghar-backend-ruddy.vercel.app";
-export const API_BASE_URL = "http://localhost:3001";
+export const API_BASE_URL = "https://sareeghar-backend-ruddy.vercel.app";
+// export const API_BASE_URL = "http://localhost:3001";
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/api/auth`,
